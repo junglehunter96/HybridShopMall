f9ddc841692e016b1ad4889a1fe75f5fc2bab7b4
junglehunter96 <junglehunter96@Gmail.com>
2019-08-12 10:14:28 +0800

    fix  swiper bugs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,104 @@
 <template>
   <div class="home">
-    Home
+    <div class="home-content-wrapper">
+      <!-- swiper -->
+      <my-swiper
+        :swiperImgs="swiperImgs"
+        :height="swiperHeight"
+      ></my-swiper>
+      <!-- 导航图标 -->
+      <mode-options></mode-options>
+      <!-- 限时秒杀 -->
+      <seconds :dataSource="secondsDatas"></seconds>
+      <!-- 活动 -->
+      <activity>
+        <div class="activity-520">
+          <img
+            v-for="(item, index) in activityDatas"
+            :key="index"
+            :src="item.icon"
+            alt=""
+          >
+        </div>
+      </activity>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    
+import mySwiper from '@c/common/MySwiper'
+import activity from '@c/currency/Activity'
+import modeOptions from '@c/currency/ModeOptions'
+import seconds from '@c/currency/Seconds'
+export default {
+  data () {
+    return {
+      swiperHeight: '184px',
+      swiperDatas: [],
+      activityDatas: [],
+      secondsDatas: []
+    }
+  },
+  components: {
+    mySwiper, activity, modeOptions, seconds
+  },
+  computed: {
+    swiperImgs () {
+      return this.swiperDatas.map(item => item.icon)
+    }
+  },
+  methods: {
+    initData () {
+      this.$http.all([
+        this.$http.get('/swiper'),
+        this.$http.get('/activitys'),
+        this.$http.get('/seconds'),
+      ]).then(this.$http.spread((swiperDatas, activityDatas, secondsDatas) => {
+        this.swiperDatas = swiperDatas.list;
+        this.activityDatas = activityDatas.list;
+        this.secondsDatas = secondsDatas.list;
+      }))
+    }
+  },
+  mounted () {
+    this.initData();
   }
+}
 </script>
 
 <style lang="scss" scoped>
 .home {
-  width: 100%;
   height: 100%;
+  overflow: hidden;
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+  }
+  &-content-wrapper {
+    height: 100%;
+
+    // 520 活动
+    .activity-520 {
+      background-color: #f5f5f5;
+      margin-top: px2rem(10);
+      border-top-left-radius: px2rem(8);
+      border-top-right-radius: px2rem(8);
+
+      img {
+        display: inline-block;
+        width: 100%;
+      }
+    }
+
+    // 拼购节
+    .activity-ping-gou-jie {
+      background-color: white;
+      margin-top: $marginSize;
+      img {
+        width: 100%;
+      }
+    }
+  }
 }
 </style>
