00b378af767e4e00b86f573d081bf04a9e8dc006
junglehunter96 <junglehunter96@Gmail.com>
2019-08-12 10:15:04 +0800

    add activity c

---
+++ src/components/currency/Activity.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="activity z-index-2">
+    <slot></slot>
+  </div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+.activity {
+  max-height: px2rem(160);
+  position: relative;
+}
+</style>
