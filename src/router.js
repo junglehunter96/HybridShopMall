3274f5b12ed6e10adc88193ab189e32fdf3d21d5
junglehunter96 <junglehunter96@Gmail.com>
2019-08-15 23:24:13 +0800

    增加缓存路由和页面切换动效

--- src/router.js
+++ src/router.js
@@ -5,11 +5,22 @@ import Main from './views/Main.vue'
 Vue.use(Router)
 
 export default new Router({
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
       component: Main
-    }
+    },
+    {
+      path: '/goodsList',
+      name: 'goodsList',
+      component: () => import('./views/GoodsList.vue')
+    },
+    {
+      path: '/gooddetails',
+      name: 'goodsDetails',
+      component: () => import('./views/GoodsDetails.vue')
+    },
   ]
 })
